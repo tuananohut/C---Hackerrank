384e04a38be110409229c9bd05288ba6b2746d3d
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-05-12 12:26:17 +0300

    Create switchcase.cs
    switch-case logic

---
+++ switchcase.cs
@@ -0,0 +1,26 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       switch(degisken)
+        {
+        case sabit1:
+            komutlar;
+            break;
+        case sabit2:
+            komutlar;
+            break;
+        case sabit3:
+            komutlar;
+            break;
+        default:
+            komutlar;
+            break;
+        }
+    }
+}
