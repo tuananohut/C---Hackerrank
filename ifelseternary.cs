db7c0b2e00717bf1a493e5af76eaaa103144b951
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-05-12 12:16:42 +0300

    Create ifelseternary.cs
    Conditionals

---
+++ ifelseternary.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        bool condition = true;
+
+        if (condition)
+        {
+            Console.WriteLine("Değişken: true");
+        }
+        
+        else
+        {
+            Console.WriteLine("Değişken: false");
+        }
+    }
+}
