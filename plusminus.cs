6c578464666f070b8657eddfbc49dde7cae2bc7c
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-05-12 12:44:35 +0300

    Create plusminus.cs
    chatgpt solved

---
+++ plusminus.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        foreach (int num in arr)
+        {
+            if (num > 0)
+            {
+                positiveCount++;
+            }
+            else if (num < 0)
+            {
+                negativeCount++;
+            }
+            else
+            {
+                zeroCount++;
+            }
+        }
+
+        int totalCount = arr.Count;
+
+        decimal positiveRatio = (decimal)positiveCount / totalCount;
+        decimal negativeRatio = (decimal)negativeCount / totalCount;
+        decimal zeroRatio = (decimal)zeroCount / totalCount;
+
+        Console.WriteLine($"{positiveRatio:F6}");
+        Console.WriteLine($"{negativeRatio:F6}");
+        Console.WriteLine($"{zeroRatio:F6}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
