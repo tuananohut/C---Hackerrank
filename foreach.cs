b064c39d8c350e1bce1e2e2614b75dddc8ac0438
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-05-12 12:39:55 +0300

    Create foreach.cs
    foreach logic

---
+++ foreach.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int[] sayiDizisi = {2,4,5,1,2,4};    
+        int toplam = 0 ;
+
+        foreach(int i in SayiDizisi)
+        {
+            toplam += i ;
+        }
+    }
+}
